ab9d841e1d1be87190a9b6a45a64a5edbf3b527c
Leandro <leandrodasilvalara@yahoo.com.br>
2021-01-07 12:16:56 -0300

    Addicionando algumas form do Bootstrap

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -8,16 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class AlunosComponent implements OnInit {
 
 
-  titulo = 'Alunos';
+  public titulo = 'Alunos';
+  public alunoSelecionado = 'alunoSelecionado';
 
   public alunos = [
-    { nome:'Paula'},
-    { nome:'Laura'},
-    { nome:'Luiza'},
-    { nome:'Lucas'},
-    { nome:'Pedro'},
-    { nome:'Paulo'},
+    { id: 1, nome:'Paula', sobrenome: 'Kent', telefone: 27513421},
+    { id: 2, nome:'Laura', sobrenome: 'Isabela', telefone: 27513451},
+    { id: 3, nome:'Luiza', sobrenome: 'Antonia', telefone: 27519991},
+    { id: 4, nome:'Lucas', sobrenome: 'Maria', telefone: 32613421},
+    { id: 5, nome:'Pedro', sobrenome: 'Machado', telefone: 26623421},
+    { id: 6, nome:'Paulo', sobrenome: 'Alvares', telefone: 27713421},
   ];
+
+  alunoSelect(aluno: any)
+  {
+    this.alunoSelecionado = aluno.nome;
+  }
+
+  voltar()
+  {
+    this.alunoSelecionado = '';
+  }
 
   constructor() { }
 
--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -8,14 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class ProfessoresComponent implements OnInit {
 
   titulo = 'Professores';
+  public professorSelecionado = 'professorSelecionado';
 
   public professores = [
-    { nome:'Lauro'},
-    { nome:'Ronaldo'},
-    { nome:'Rodrigo'},
-    { nome:'Alaxendre'},
-    { nome: 'Roberto' },
+    {id:1, nome:'Lauro', disciplina: 'Matemática'},
+    {id:1, nome:'Ronaldo', disciplina: 'Física'},
+    {id:1, nome:'Rodrigo', disciplina: 'PortuguÊs'},
+    {id:1, nome:'Alaxendre', disciplina: 'Inglês'},
+    {id:1, nome: 'Roberto', disciplina: 'Programação' },
   ];
+
+  professorSelect(aluno: any)
+  {
+    this.professorSelecionado = aluno.nome;
+  }
+
+  voltar()
+  {
+    this.professorSelecionado = '';
+  }
 
   constructor() { }
 
